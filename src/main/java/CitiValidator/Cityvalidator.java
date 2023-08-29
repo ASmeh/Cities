a3b7044f677627f9e95435c244d271f8d1bcfa4d
Oleksii Samoliuk <samoluk.aleksey@gmail.com>
2023-08-29 17:03:38 +0300

    Added scetch for city processing.

---
+++ src/main/java/CitiValidator/Cityvalidator.java
@@ -0,0 +1,14 @@
+package CitiValidator;
+
+import CityGetter.*;
+import CityGetter.CittyGetterByFile;
+
+public class Cityvalidator {
+    private CittyGetter cityGetter = new CittyGetterByFile();
+
+    public boolean isValidCity() {
+        //String city = cityGetter.getCity();
+        return true;
+
+    }
+}
---
+++ src/main/java/CityGetter/CittyGetter.java
@@ -0,0 +1,21 @@
+package CityGetter;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class CittyGetter {
+    HashSet<String> citiesToProcess = new HashSet<>();
+    //HashSet<String> citiesCatalog = new HashSet<>();
+
+    String getCity(char letter) {
+        List<String> citiesWithCriteria = citiesToProcess.stream().
+                filter(city -> city.toLowerCase().charAt(0) == letter)
+                .collect(Collectors.toList());
+        if(citiesWithCriteria.size() != 0) {
+            citiesToProcess.remove(citiesWithCriteria.get(0));
+            return citiesWithCriteria.get(0);
+        }
+        return "";
+    }
+}
